f6ca31cbc404c89aa03b9e0bcb5ba09572646cbd
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-18 23:18:41 -0500

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/with-contenv bash
-iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
-ip6tables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
+#iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
+#ip6tables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
+cloudflared --origincert /config/cert.pem --config /config/config.yml service install
+service cloudflared start
 sleep infinity
