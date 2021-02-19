ec7c2683da3c033b7b42c64001d96232a59ff782
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-18 23:23:31 -0500

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -2,5 +2,6 @@
 #iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
 #ip6tables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
 cloudflared --origincert /config/cert.pem --config /config/config.yml service install
+chkconfig cloudflared on
 service cloudflared start
 sleep infinity
