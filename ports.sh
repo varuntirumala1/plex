9b87b23456871b50d908b06d78c3dc291c0df2b0
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-25 00:04:42 -0500

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -3,4 +3,4 @@
 #ip6tables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
 #cloudflared --origincert /config/cert.pem --config /config/config.yml service install
 
- cloudflared --origincert /config/cert.pem --config /config/config.yml tunnel run -p http2 plex
+ cloudflared --origincert /argo/cert.pem --config /argo/config.yml tunnel run -p http2 plex
