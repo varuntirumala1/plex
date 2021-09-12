50592f21268640d56c81b6d9228aef931773818b
varuntirumala1 <varun.tirumala@gmail.com>
2021-09-12 14:58:08 -0400

    Delete ports.sh

--- ports.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bash
-#iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
-#ip6tables -t nat -I PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 32400
-#cloudflared --origincert /config/cert.pem --config /config/config.yml service install
-
- cloudflared --origincert /argo/cert.pem --config /argo/config.yml tunnel run -p http2 plex
